8e7ab1db48b46ea44ffb7233aac146307f9da070
Palekar <renuka.palekar@hpe.com>
2024-08-02 19:00:11 +0530

    added simple matlab

---
+++ simple_matlab/hp.py
@@ -0,0 +1,8 @@
+def matlabs():
+    hp_detail= input('hp laptop details ')
+    ram= input('ram')
+    price= input('price')
+
+    matlabs= f"Hye my hp laptop has {ram} ram and i brought it {price} lac. "
+
+    print(matlabs)
